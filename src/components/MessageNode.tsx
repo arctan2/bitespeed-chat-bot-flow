274c150e40babd81ecb8c979a95db72f60957610
arctan2 <prateekbm005@gmail.com>
2025-07-18 17:19:15 +0530

    restricting target start selection and added icons

--- src/components/MessageNode.tsx
+++ src/components/MessageNode.tsx
@@ -1,9 +1,11 @@
 import "./MessageNode.css";
 import { Handle, Position } from '@xyflow/react';
+import ChatIcon from "../assets/icons/chat.png";
+import WhatsappIcon from "../assets/icons/WhatsApp.webp";
 
 interface MessageNodeProps {
 	id: string,
-	data: { 
+	data: {
 		msg: string
 	}
 }
@@ -11,10 +13,18 @@ interface MessageNodeProps {
 function MessageNode(props: MessageNodeProps) {
 	return (
 		<div className="text-message-node">
-			<section>Send Message</section>
-			<p className="message-body">{ props.data.msg }</p>
+			<section className="node-title">
+				<div className="icon">
+					<img src={ChatIcon} />
+				</div>
+				<div>Send Message</div>
+				<div className="icon">
+					<img src={WhatsappIcon} />
+				</div>
+			</section>
+			<p className="message-body">{props.data.msg}</p>
 			<Handle type="source" position={Position.Right} />
-			<Handle type="target" position={Position.Left} />
+			<Handle type="target" position={Position.Left} isConnectableStart={false} />
 		</div>
 	);
 }
